0d7acc1d001246381e3cf26a632fa99432f6091e
kameshsaripaka <saripaka.kamesh@gmail.com>
2025-10-04 20:17:22 +0530

    world commit

--- src/main/java/com/example/HelloWorldApp/App.java
+++ src/main/java/com/example/HelloWorldApp/App.java
@@ -5,6 +5,6 @@ package com.example.HelloWorldApp;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Word Energy Day!");
+        System.out.println("World Energy Day!");
     }
 }
